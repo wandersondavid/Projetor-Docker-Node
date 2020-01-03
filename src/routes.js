e2873d0bd094d359fb3cae8e46d32519acd60fac
Wanderson David <wandersondavid.ifg@gmail.com>
2020-01-03 16:41:31 -0300

    server confugurado

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import {Router} from 'express';
+
+const routes = new Router();
+
+routes.get('/r', (req, res)=>{
+    return res.json({msg:'Hello World'});
+}),
+routes.get('/', (req, res)=>{
+    return res.json({msg:'Hello World'});
+})
+
+
+export default routes;
