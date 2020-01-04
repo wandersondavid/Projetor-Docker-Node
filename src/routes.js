4c79483ec5ff74c4472fd3865fde6212d0a418fd
Wanderson David <wandersondavid.ifg@gmail.com>
2020-01-04 18:42:22 -0300

    Cadastro de Users

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,9 @@
 import {Router} from 'express';
-import User from './app/models/User'
+
+import UserController from './app/controllers/UserController'
+
 const routes = new Router();
 
-routes.get('/', async (req, res)=>{
-    const user = await User.create({
-        name: 'Wanderson David',
-        email:'wanderson@gmail.com',
-        password_hash: '1234256436',
-    })
-    return res.json(user);
-})
+routes.post('/users', UserController.store);
 
 export default routes;
